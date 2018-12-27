c65895c4687c00f287587125594581c1e249db48
Anna Kowalczuk <anna@kowalczuk.eu>
2018-12-27 23:09:21 +0100

    Updates

---
+++ precache-manifest.550c84a23f6841b2e1d4fb2c56cb6af4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83733084fa38ab565e00",
+    "url": "/react-egghead-tutorial/static/js/runtime~main.83733084.js"
+  },
+  {
+    "revision": "2cecf8b9ec5bfdf87d6a",
+    "url": "/react-egghead-tutorial/static/js/main.2cecf8b9.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-egghead-tutorial/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "2cecf8b9ec5bfdf87d6a",
+    "url": "/react-egghead-tutorial/static/css/main.62640880.chunk.css"
+  },
+  {
+    "revision": "71ae113386dec59eb0fbbcf512864671",
+    "url": "/react-egghead-tutorial/index.html"
+  }
+];
